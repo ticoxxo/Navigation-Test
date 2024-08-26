f101d44ac96708070e0682fc5555e2532a4d85ef
Ticoxxo <ticoxxo@gmail.com>
2024-08-26 09:46:47 -0700

    Initial Commit

---
+++ Navigation Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation Test
+//
+//  Created by Alberto Almeida on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation Test/Navigation_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_TestApp.swift
+//  Navigation Test
+//
+//  Created by Alberto Almeida on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
